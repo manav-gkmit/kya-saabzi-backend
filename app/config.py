c5d6a15146236abfc112056c28a71760e525f673
Manav Sharma <manav@gkmit.co>
2025-11-18 00:37:40 +0530

    Update access token expiry to 30 min

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 20
 
     DATABASE_URL: str
     FRONTEND_URL: str
