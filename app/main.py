4dbfbea06063f46ee9f79ebf0b274b1f561aa73d
Manav Sharma <manav@gkmit.co>
2025-11-17 20:11:57 +0530

    chore: Update main.py to be in app/ directory instead

--- app/main.py
+++ app/main.py
@@ -1,23 +1,13 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.api import auth, add_dish, get_logs, recommendation
+from app.api import auth, add_dish, get_logs
 
 
 app = FastAPI()
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(auth.router)
 app.include_router(add_dish.router)
 app.include_router(get_logs.router)
-app.include_router(recommendation.router)
 
 
 @app.get("/")
