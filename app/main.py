a19b47978b00488402f8999d306686090d4580cd
Manav Sharma <manav@gkmit.co>
2025-11-21 16:52:36 +0530

    Add health check endpoint

--- app/main.py
+++ app/main.py
@@ -26,3 +26,8 @@ app.include_router(recommendation.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "Healthy"}
